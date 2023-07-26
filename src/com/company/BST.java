31aea2b070a8b4b5f223454ea98a0576158e765b
Aikusya <butterflyy1211@gmail.com>
2023-07-26 15:33:32 +0600

    full3

--- src/com/company/BST.java
+++ src/com/company/BST.java
@@ -1,17 +1,15 @@
 package com.company;
 
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class BST<K extends Comparable<K>, V> {
+public class BST<K extends Comparable<K>, V> implements Iterable<Map.Entry<K, V>> {
     private Node root;
     private int size;
-    private class Node {
+
+    private static class Node<K, V> {
         private K key;
         private V value;
-        private Node left, right;
+        private Node<K, V> left, right;
 
         public Node(K key, V value) {
             this.key = key;
@@ -19,46 +17,46 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
-    BST(){
+    public BST() {
         this.size = 0;
     }
 
-    public int getSize(int size) {
+    public int getSize() {
         return size;
     }
 
-    public void put(K key, V value){
+    public void put(K key, V value) {
         root = put(root, key, value);
     }
 
-    private Node put(Node node, K key, V value){
-        if(node == null){
+    private Node<K, V> put(Node<K, V> node, K key, V value) {
+        if (node == null) {
             size++;
-            return new Node(key, value);
+            return new Node<>(key, value);
         }
 
         int cmp = key.compareTo(node.key);
-        if(cmp < 0){
+        if (cmp < 0) {
             node.left = put(node.left, key, value);
         } else if (cmp > 0) {
             node.right = put(node.right, key, value);
         } else {
             node.value = value;
         }
-            return node;
+        return node;
     }
 
-    public V get(K key){
-        return get(root, key);
+    public V get(K key) {
+        return (V) get(root, key);
     }
 
-    private V get(Node node, K key){
-        if(node == null){
+    private V get(Node<K, V> node, K key) {
+        if (node == null) {
             return null;
         }
 
         int cmp = key.compareTo(node.key);
-        if(cmp < 0){
+        if (cmp < 0) {
             return get(node.left, key);
         } else if (cmp > 0) {
             return get(node.right, key);
@@ -67,50 +65,51 @@ public class BST<K extends Comparable<K>, V> {
         }
     }
 
-    public void delete(K key){
+    public void delete(K key) {
         root = delete(root, key);
     }
 
-    private Node delete(Node node, K key){
-        if(node == null){
+    private Node<K, V> delete(Node<K, V> node, K key) {
+        if (node == null) {
             return null;
         }
 
         int cmp = key.compareTo(node.key);
-        if(cmp < 0){
-            return delete(node.left, key);
+        if (cmp < 0) {
+            node.left = delete(node.left, key);
         } else if (cmp > 0) {
-            return delete(node.right, key);
+            node.right = delete(node.right, key);
         } else {
             size--;
-            if(node.left == null){
+            if (node.left == null) {
                 return node.right;
             } else if (node.right == null) {
                 return node.left;
             } else {
-                Node minRight = findMin(node.right);
+                Node<K, V> minRight = findMin(node.right);
                 node.key = minRight.key;
-                node.value = minRight.value;;
+                node.value = minRight.value;
                 node.right = delete(node.right, minRight.key);
             }
         }
         return node;
     }
 
-    private Node findMin(Node node){
-        while (node.left != null){
+    private Node<K, V> findMin(Node<K, V> node) {
+        while (node.left != null) {
             node = node.left;
         }
         return node;
     }
 
-    public Iterable<Map.Entry<K, V>> iterator() {
+    @Override
+    public Iterator<Map.Entry<K, V>> iterator() {
         List<Map.Entry<K, V>> entries = new ArrayList<>();
         inorderTraversal(root, entries);
-        return entries;
+        return entries.iterator();
     }
 
-    private void inorderTraversal(Node node, List<Map.Entry<K, V>> entries) {
+    private void inorderTraversal(Node<K, V> node, List<Map.Entry<K, V>> entries) {
         if (node != null) {
             inorderTraversal(node.left, entries);
             entries.add(new AbstractMap.SimpleEntry<>(node.key, node.value));
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,22 +4,22 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-
         BST<Integer, String> bst = new BST<>();
         bst.put(5, "five");
         bst.put(3, "three");
         bst.put(7, "seven");
         bst.put(10, "ten");
 
-        //System.out.println(bst.get(3));
-        //bst.delete(5);
+        // Testing get() and delete()
+        System.out.println("Value at key 3: " + bst.get(3));
+        bst.delete(5);
 
-
-
-        for (Map.Entry<Integer, String> entry : bst.iterator()) {
+        // Printing the tree using iterator()
+        for (Map.Entry<Integer, String> entry : bst) {
             Integer key = entry.getKey();
             String value = entry.getValue();
             System.out.println("Key: " + key + ", Value: " + value);
         }
     }
 }
+
