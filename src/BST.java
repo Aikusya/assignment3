bd98dfc80718945b524f089f801546d9e82e52cd
Aikusya <butterflyy1211@gmail.com>
2023-07-26 12:53:39 +0600

    iterable

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 public class BST<K extends Comparable<K>, V> {
     private Node root;
     private int size;
@@ -88,8 +90,18 @@ public class BST<K extends Comparable<K>, V> {
             return node;
         }
 
-        public Iterable<K> iterator(){
+        public Iterable<K> iterator() {
+            List<K> keys = new ArrayList<>();
+            inorderTraversal(root, keys);
+            return keys;
+        }
 
+        private void inorderTraversal(Node node, List<K> keys) {
+            if (node != null) {
+                inorderTraversal(node.left, keys);
+                keys.add(node.key);
+                inorderTraversal(node.right, keys);
+            }
         }
 
         public int getSize(int size){
